1182879316d4723c0e5e8b02818a6dcea33acb01
alv00 <dingjing0604@outlook.com>
2022-11-23 16:54:02 +0800

    volume can be empty

--- apis/cloud/v1/obcluster_types.go
+++ apis/cloud/v1/obcluster_types.go
@@ -43,7 +43,7 @@ type ResourcesSpec struct {
 	CPU     resource.Quantity `json:"cpu"`
 	Memory  resource.Quantity `json:"memory"`
 	Storage []StorageSpec     `json:"storage"`
-	Volume  VolumeSpec        `json:"volume"`
+	Volume  VolumeSpec        `json:"volume,omitempty"`
 }
 
 type VolumeSpec struct {
