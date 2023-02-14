56653d6a4355fef26c11a8cfc3781ee7c3d4307a
alv00 <dingjing0604@outlook.com>
2023-02-14 13:16:53 +0800

    Add Upgrade OB 3.X e2e

--- test/e2e/observer/rolling_upgrade.go
+++ test/e2e/observer/rolling_upgrade.go
@@ -23,7 +23,6 @@ import (
 	myconfig "github.com/oceanbase/ob-operator/pkg/config"
 
 	observerconst "github.com/oceanbase/ob-operator/pkg/controllers/observer/const"
-	"github.com/oceanbase/ob-operator/pkg/controllers/observer/model"
 	testconverter "github.com/oceanbase/ob-operator/test/e2e/converter"
 	testresource "github.com/oceanbase/ob-operator/test/e2e/resource"
 	testutils "github.com/oceanbase/ob-operator/test/e2e/utils"
@@ -35,15 +34,9 @@ var _ = ginkgo.Describe("obcluster upgrade test pipeline", func() {
 
 	var _ = ginkgo.Describe("create obcluster pipeline", func() {
 
-		var obServerList []model.AllServer
-		var service corev1.Service
-		obcluster := testconverter.GetObjFromYaml("./data/obcluster-3-v311.yaml")
+		obcluster := testconverter.GetObjFromYaml("./data/obcluster-3-1-v311.yaml")
 		obclusterNamespace := obcluster.GetNamespace()
 		obclusterName := obcluster.GetName()
-		statefulappName := fmt.Sprintf("sapp-%s", obcluster.GetName())
-		serviceName := fmt.Sprintf("svc-%s", obcluster.GetName())
-		rootserviceName := fmt.Sprintf("rs-%s", obcluster.GetName())
-		obzoneName := fmt.Sprintf("obzone-%s", obcluster.GetName())
 
 		ginkgo.It("step: create obcluster", func() {
 			err := testresource.TestClient.CreateObj(obcluster)
@@ -53,103 +46,12 @@ var _ = ginkgo.Describe("obcluster upgrade test pipeline", func() {
 			)
 		})
 
-		ginkgo.It("step: check obcluster bootstrap status", func() {
-			gomega.Eventually(func() string {
-				return testresource.TestClient.GetOBClusterStatus(obclusterNamespace, obclusterName)
-			}, OBClusterBootstrapTimeout, TryInterval).Should(
-				gomega.Equal(observerconst.TopologyReady),
-				"obcluster %s bootstrap ready", obclusterName,
-			)
-		})
-
-		ginkgo.It("step: check statefulapp status for obcluster", func() {
-			gomega.Eventually(func() bool {
-				return testresource.TestClient.JudgeStatefulappInstanceIsReadyByObj(obclusterNamespace, statefulappName)
-			}, OBClusterReadyTimeout, TryInterval).Should(
-				gomega.Equal(true),
-				"statefulapp %s is ready", statefulappName,
-			)
-		})
-
 		ginkgo.It("step: check obcluster status", func() {
-			gomega.Eventually(func() bool {
-				return testresource.TestClient.JudgeOBClusterInstanceIsReadyByObj(obclusterNamespace, obclusterName)
-			}, OBClusterReadyTimeout, TryInterval).Should(
-				gomega.Equal(true),
-				"obcluster %s is ready", obclusterName,
-			)
-		})
-
-		ginkgo.It("step: check service status for obcluster", func() {
-			var err error
-			gomega.Eventually(func() bool {
-				return testresource.TestClient.JudgeServicefForOBClusterIsReadyByObj(obclusterNamespace, serviceName)
-			}, OBClusterReadyTimeout, TryInterval).Should(
-				gomega.Equal(true),
-				"service %s is ready", serviceName,
-			)
-			service, err = testresource.TestClient.GetService(obclusterNamespace, serviceName)
-			gomega.Expect(err).To(
-				gomega.BeNil(),
-				"get service %s succeed", serviceName,
-			)
-		})
-
-		ginkgo.It("step: check observer status", func() {
-			sqlOperator := testutils.NewDefaultSqlOperator(service.Spec.ClusterIP)
-			gomega.Eventually(func() bool {
-				obServerList = sqlOperator.GetOBServer()
-				if len(obServerList) == 0 {
-					return false
-				}
-				for _, observer := range obServerList {
-					if observer.Status != observerconst.OBServerActive || observer.StartServiceTime <= 0 {
-						return false
-					}
-				}
-				return true
-			}, OBClusterReadyTimeout, TryInterval).Should(
-				gomega.Equal(true),
-				"all observer is active",
-			)
-			status := testconverter.JudgeAllOBServerStatusByObj(obServerList, obcluster)
-			gomega.Expect(status).To(
-				gomega.Equal(true),
-				"check observer and obcluster matched",
-			)
-		})
-
-		ginkgo.It("step: check rootservice status", func() {
-			rootservice, err := testresource.TestClient.GetRootService(obclusterNamespace, rootserviceName)
-			gomega.Expect(err).To(
-				gomega.BeNil(),
-				"get rootservice %s succeed", rootserviceName,
-			)
-			sqlOperator := testutils.NewDefaultSqlOperator(service.Spec.ClusterIP)
-			rsList := sqlOperator.GetRootService()
-			status := testconverter.JudgeRootserviceStatusByObj(rsList, rootservice)
-			gomega.Expect(status).To(
-				gomega.Equal(true),
-				"rootservice %s status is ok", rootserviceName,
-			)
-		})
-
-		ginkgo.It("step: check obzone status", func() {
-
-			gomega.Eventually(func() bool {
-				_, err := testresource.TestClient.GetOBZone(obclusterNamespace, obzoneName)
-				return err == nil
-			}, OBClusterUpdateTReadyimeout, TryInterval).Should(
-				gomega.Equal(true),
-				"get obzone %s succeed", obzoneName,
-			)
-
-			obzone, _ := testresource.TestClient.GetOBZone(obclusterNamespace, obzoneName)
-
-			status := testconverter.JudgeOBzoneStatusByObj(obServerList, obzone)
-			gomega.Expect(status).To(
-				gomega.Equal(true),
-				"obzone %s status is ok", obzoneName,
+			gomega.Eventually(func() string {
+				return testresource.TestClient.GetOBStatusClusterStatus(obclusterNamespace, obclusterName, myconfig.ClusterName)
+			}, OBClusterReadyimeout, TryInterval).Should(
+				gomega.Equal(observerconst.ClusterReady),
+				"obcluster %s ready", obclusterName,
 			)
 		})
 
@@ -158,7 +60,7 @@ var _ = ginkgo.Describe("obcluster upgrade test pipeline", func() {
 	var _ = ginkgo.Describe("upgrade obcluster pipeline", func() {
 
 		var service corev1.Service
-		obcluster := testconverter.GetObjFromYaml("./data/obcluster-3-v314.yaml")
+		obcluster := testconverter.GetObjFromYaml("./data/obcluster-3-1-v314.yaml")
 		obclusterNamespace := obcluster.GetNamespace()
 		obclusterName := obcluster.GetName()
 		serviceName := fmt.Sprintf("svc-%s", obcluster.GetName())
@@ -219,8 +121,7 @@ var _ = ginkgo.Describe("obcluster upgrade test pipeline", func() {
 			targetVersion := "3.1.4"
 			gomega.Eventually(func() bool {
 				version := sqlOperator.GetVersion()
-				currentVersion := version[0].Version
-				return currentVersion == targetVersion
+				return len(version) != 0 && version[0].Version == targetVersion
 			}, OBClusterUpdateTReadyimeout, TryInterval).Should(
 				gomega.Equal(true),
 				"obcluster upgrade succeed",
