02bdbce99a32e5aa56f70b45538f6f7399a9ce70
Powerfool <yuyi.wsy@oceanbase.com>
2023-11-29 20:00:43 +0800

    Actions for building and releasing manifests (#107)
    * chore: workflows for building and release

--- distribution/obproxy/build.sh
+++ distribution/obproxy/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t $1:$2-$3 --build-arg VERSION=$2 --build-arg RELEASE=$3 --build-arg ARCH=$4 .
+# @Params TARGETPLATFORM linux/amd64 or linux/arm64
+# VERSION: x.y.z-r e.g. 4.2.1-11 which merge old VERSION and RELEASE
+docker build -t $1:$2 --build-arg VERSION=$2 --build-arg TARGETPLATFORM=$3 .
