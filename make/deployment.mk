ad1c5e7e4543dd83b35219536d634792cd6af2a3
yuyi <yuyi.wsy@oceanbase.com>
2023-12-20 19:41:02 +0800

    chore(manifests): delete config/dev

--- make/deployment.mk
+++ make/deployment.mk
@@ -21,15 +21,6 @@ deploy: manifests kustomize ## Deploy controller to the K8s cluster specified in
 undeploy: ## Undeploy controller from the K8s cluster specified in ~/.kube/config. Call with ignore-not-found=true to ignore resource not found errors during deletion.
 	$(KUSTOMIZE) build config/default | kubectl delete --ignore-not-found=$(ignore-not-found) -f -
 
-.PHONY: deploy-dev
-deploy: manifests kustomize ## Deploy dev mode controller to the K8s cluster specified in ~/.kube/config.
-	cd config/manager && $(KUSTOMIZE) edit set image controller=${IMG}
-	$(KUSTOMIZE) build config/dev | kubectl apply -f -
-
-.PHONY: undeploy-dev
-deploy: manifests kustomize ## Undeploy dev mode controller from the K8s cluster specified in ~/.kube/config
-	$(KUSTOMIZE) build config/dev | kubectl delete --ignore-not-found=$(ignore-not-found) -f -
-
 .PHONY: redeploy
 redeploy: undeploy uninstall export-crd export-operator install deploy ## redeploy crd and controller to the K8s cluster specified in ~/.kube/config.
 
