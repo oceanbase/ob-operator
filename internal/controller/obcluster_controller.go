f192ecbf8cbb01d1c651bf2e08c66b8fe020c9a6
yuyi <yuyi.wsy@oceanbase.com>
2024-02-28 20:11:42 +0800

    fix(ctrl): remove owns(obparameter) in obcluster controller

--- internal/controller/obcluster_controller.go
+++ internal/controller/obcluster_controller.go
@@ -97,7 +97,7 @@ func (r *OBClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 func (r *OBClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.OBCluster{}).
-		Owns(&v1alpha1.OBZone{}).Owns(&v1alpha1.OBParameter{}).
+		Owns(&v1alpha1.OBZone{}).
 		WithEventFilter(preds).
 		Complete(r)
 }
