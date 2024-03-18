83046762fb7c9cf70c4b9a2c30eb232c5123f154
yuyi <yuyi.wsy@oceanbase.com>
2024-03-18 16:16:54 +0800

    feat(task): add flow builder and task builder

---
+++ pkg/task/builder/flow_builder.go
@@ -0,0 +1,111 @@
+/*
+Copyright (c) 2023 OceanBase
+ob-operator is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
+package builder
+
+import (
+	"errors"
+
+	"github.com/oceanbase/ob-operator/pkg/task/const/strategy"
+	tasktypes "github.com/oceanbase/ob-operator/pkg/task/types"
+	tt "github.com/oceanbase/ob-operator/pkg/task/types"
+)
+
+type FlowGenerator[T any] func(T) *tt.TaskFlow
+type FlowMap[T any] map[tt.FlowName]FlowGenerator[T]
+
+func NewFlowMap[T any]() FlowMap[T] {
+	return make(map[tt.FlowName]FlowGenerator[T])
+}
+
+func (f FlowMap[T]) Register(name tt.FlowName, flow FlowGenerator[T]) {
+	f[name] = flow
+}
+
+func (f FlowMap[T]) Get(name tt.FlowName, resource T) (*tt.TaskFlow, error) {
+	gen, ok := f[name]
+	if !ok {
+		return nil, errors.New("TaskFlow not found: " + string(name))
+	}
+	return gen(resource), nil
+}
+
+type FlowBuilder interface {
+	Build() *tasktypes.TaskFlow
+	Step(task tasktypes.TaskName) FlowBuilder
+	Steps(tasks ...tasktypes.TaskName) FlowBuilder
+	To(status string) FlowBuilder
+	FailedTo(status string) FlowBuilder
+	RetryStrategy(strategy tasktypes.TaskFailureStrategy) FlowBuilder
+	MaxRetry(max int) FlowBuilder
+}
+
+type flowBuilder struct {
+	operationContext *tasktypes.OperationContext
+}
+
+func NewFlowBuilder(name tasktypes.FlowName) FlowBuilder {
+	return &flowBuilder{
+		operationContext: &tasktypes.OperationContext{
+			Name:  name,
+			Tasks: []tasktypes.TaskName{},
+		},
+	}
+}
+
+func (b *flowBuilder) Build() *tasktypes.TaskFlow {
+	if b.operationContext.TargetStatus == "" {
+		b.operationContext.TargetStatus = "running"
+	}
+	if b.operationContext.OnFailure.Strategy == "" {
+		b.operationContext.OnFailure.Strategy = strategy.StartOver
+	}
+	if b.operationContext.OnFailure.MaxRetry == 0 {
+		b.operationContext.OnFailure.MaxRetry = 32
+	}
+	if b.operationContext.OnFailure.NextTryStatus == "" {
+		b.operationContext.OnFailure.NextTryStatus = "failed"
+	}
+	return &tasktypes.TaskFlow{
+		OperationContext: b.operationContext,
+	}
+}
+
+func (b *flowBuilder) Step(task tasktypes.TaskName) FlowBuilder {
+	b.operationContext.Tasks = append(b.operationContext.Tasks, task)
+	return b
+}
+
+func (b *flowBuilder) To(status string) FlowBuilder {
+	b.operationContext.TargetStatus = status
+	return b
+}
+
+func (b *flowBuilder) FailedTo(status string) FlowBuilder {
+	b.operationContext.OnFailure.NextTryStatus = status
+	return b
+}
+
+func (b *flowBuilder) RetryStrategy(strategy tasktypes.TaskFailureStrategy) FlowBuilder {
+	b.operationContext.OnFailure.Strategy = strategy
+	return b
+}
+
+func (b *flowBuilder) MaxRetry(max int) FlowBuilder {
+	b.operationContext.OnFailure.MaxRetry = max
+	return b
+}
+
+func (b *flowBuilder) Steps(tasks ...tasktypes.TaskName) FlowBuilder {
+	b.operationContext.Tasks = append(b.operationContext.Tasks, tasks...)
+	return b
+}
---
+++ pkg/task/builder/task_builder.go
@@ -0,0 +1,56 @@
+/*
+Copyright (c) 2023 OceanBase
+ob-operator is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
+package builder
+
+import (
+	"errors"
+
+	tt "github.com/oceanbase/ob-operator/pkg/task/types"
+)
+
+type TypedTask[T any] func(T) tt.TaskError
+type TaskMap[T any] map[tt.TaskName]TypedTask[T]
+
+func NewTaskMap[T any]() TaskMap[T] {
+	return make(map[tt.TaskName]TypedTask[T])
+}
+
+func (t TaskMap[T]) Register(name tt.TaskName, task TypedTask[T]) {
+	t[name] = task
+}
+
+func (t TaskMap[T]) Get(name tt.TaskName) (TypedTask[T], error) {
+	task, ok := t[name]
+	if !ok {
+		return nil, errors.New("Task not found: " + string(name))
+	}
+	return task, nil
+}
+
+func (t TaskMap[T]) GetLegacyTask(name tt.TaskName, resource T) (tt.TaskFunc, error) {
+	task, ok := t[name]
+	if !ok {
+		return nil, errors.New("Task not found: " + string(name))
+	}
+	return func() tt.TaskError {
+		return task(resource)
+	}, nil
+}
+
+func (t TaskMap[T]) Run(name tt.TaskName, resource T) tt.TaskError {
+	task, err := t.Get(name)
+	if err != nil {
+		return err
+	}
+	return task(resource)
+}
