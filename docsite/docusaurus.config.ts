06f39cc2f7cbcecac020be2018460aa56a7754c9
yuyi <yuyi.wsy@oceanbase.com>
2024-04-17 15:55:05 +0800

    fix(docs): fixed wrong links and update site keywords

--- docsite/docusaurus.config.ts
+++ docsite/docusaurus.config.ts
@@ -48,6 +48,9 @@ const config: Config = {
 
   trailingSlash: false,
   themeConfig: {
+    metadata: [
+      { name: 'keywords', content: 'oceanbase, kubernetes, operator, manual, deployment, backup, database, distributed database, mysql, sql, cloud native' },
+    ],
     algolia: {
       appId: '6JQM9QDU5V',
       apiKey: '75f5591a502e47777a08a02b96bc09a1',
