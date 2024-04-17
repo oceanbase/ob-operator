2d2168fc2413a5fb9004fb81b7b3cd65029b3e24
yuyi <yuyi.wsy@oceanbase.com>
2024-04-17 15:39:24 +0800

    fix(docs): corrected links for Github in menubar and edit this page in doc page

--- docsite/docusaurus.config.ts
+++ docsite/docusaurus.config.ts
@@ -37,15 +37,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/oceanbase/ob-operator/tree/master/docsite',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -94,7 +86,7 @@ const config: Config = {
           position: 'right',
         },
         {
-          href: 'https://github.com/oceanbase',
+          href: 'https://github.com/oceanbase/ob-operator',
           label: 'GitHub',
           position: 'right',
         },
