395b446978fb8c33b435744114ac50cd983d1f30
宴晖 <2689991790@qq.com>
2024-04-30 14:41:28 +0800

    Encapsulate the AlarmFilter component

--- ui/src/pages/Alert/Rules/index.tsx
+++ ui/src/pages/Alert/Rules/index.tsx
@@ -1,3 +1,13 @@
-export default function Rules(){
-    return null
+import { Card, Form } from 'antd';
+import AlarmFilter from '../AlarmFilter';
+
+export default function Rules() {
+  const [form] = Form.useForm();
+  return (
+    <div>
+      <Card>
+        <AlarmFilter form={form} type="rules" />
+      </Card>
+    </div>
+  );
 }
--- ui/src/pages/Alert/Shield/index.tsx
+++ ui/src/pages/Alert/Shield/index.tsx
@@ -1,3 +1,13 @@
-export default function Shield(){
-    return null
+import { Card, Form } from 'antd';
+import AlarmFilter from '../AlarmFilter';
+
+export default function Shield() {
+  const [form] = Form.useForm();
+  return (
+    <div>
+      <Card>
+        <AlarmFilter form={form} type="shield" />
+      </Card>
+    </div>
+  );
 }
