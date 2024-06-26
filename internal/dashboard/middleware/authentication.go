56a86983cefaeb460e82aabdce59c255e37ac915
powerfool <yuyi.wsy@oceanbase.com>
2024-06-26 15:03:28 +0800

    fix(odc): skip session validation for auth/ api (#471)

--- internal/dashboard/middleware/authentication.go
+++ internal/dashboard/middleware/authentication.go
@@ -28,7 +28,10 @@ import (
 
 func LoginRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if strings.HasSuffix(c.Request.RequestURI, "login") || strings.HasSuffix(c.Request.RequestURI, "info") || strings.HasSuffix(c.Request.RequestURI, "webhook/alert/log") {
+		if strings.HasSuffix(c.Request.RequestURI, "login") ||
+			strings.HasSuffix(c.Request.RequestURI, "info") ||
+			strings.HasSuffix(c.Request.RequestURI, "webhook/alert/log") ||
+			strings.HasPrefix(c.Request.RequestURI, "/api/v1/auth/") {
 			c.Next()
 			return
 		}
