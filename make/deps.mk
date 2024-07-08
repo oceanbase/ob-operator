f5b45019130e6966e4ca8db4197640dc29bd8179
Ziyi Li <58327725+lizzy-0323@users.noreply.github.com>
2024-07-08 17:06:16 +0800

    fix: bump controller-gen to v0.15.0 (#484)

--- make/deps.mk
+++ make/deps.mk
@@ -11,7 +11,7 @@ ENVTEST ?= $(LOCALBIN)/setup-envtest
 
 ## Tool Versions
 KUSTOMIZE_VERSION ?= v5.0.0
-CONTROLLER_TOOLS_VERSION ?= v0.13.0 # v0.11.3 can not support webhook manifests' generation, update to v0.13.0
+CONTROLLER_TOOLS_VERSION ?= v0.15.0 # v0.13.0 will cause Panic, update to v0.15.0
 
 KUSTOMIZE_INSTALL_SCRIPT ?= "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
 .PHONY: kustomize
