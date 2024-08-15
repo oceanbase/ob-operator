9ab73ac1cd885de979d0b419837ea5c002ff0467
宴晖 <2689991790@qq.com>
2024-08-15 13:43:08 +0800

    fix

--- ui/src/pages/Layouts/StatisticsLayout/index.tsx
+++ ui/src/pages/Layouts/StatisticsLayout/index.tsx
@@ -32,8 +32,6 @@ export default function StatisticsLayout() {
   useEffect(() => {
     if (initialState?.accountInfo?.needReset) {
       navigate('/reset');
-    } else {
-      navigate('/overview');
     }
   }, [initialState]);
 
