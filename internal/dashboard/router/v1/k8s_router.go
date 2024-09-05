b649551d72d91fe47c1d6622039af501b8db9c03
yuyi <yuyi.wsy@oceanbase.com>
2024-09-05 16:56:09 +0800

    fix(ac): loose permission for list k8s resources

--- internal/dashboard/router/v1/k8s_router.go
+++ internal/dashboard/router/v1/k8s_router.go
@@ -19,10 +19,24 @@ import (
 	h "github.com/oceanbase/ob-operator/internal/dashboard/handler"
 )
 
+// Users with read permission for cluster and obproxy can list events, too
+var eventGuard = acbiz.OR(
+	acbiz.PathGuard(string(acbiz.DomainSystem), "*", string(acbiz.ActionRead)),
+	acbiz.PathGuard(string(acbiz.DomainOBCluster), "*", string(acbiz.ActionRead)),
+	acbiz.PathGuard(string(acbiz.DomainOBProxy), "*", string(acbiz.ActionRead)),
+)
+
+// Users with write permission for cluster and obproxy can list namespaces and storage classes
+var k8sResourceGuard = acbiz.OR(
+	acbiz.PathGuard(string(acbiz.DomainSystem), "*", string(acbiz.ActionRead)),
+	acbiz.PathGuard(string(acbiz.DomainOBCluster), "*", string(acbiz.ActionWrite)),
+	acbiz.PathGuard(string(acbiz.DomainOBProxy), "*", string(acbiz.ActionWrite)),
+)
+
 func InitK8sRoutes(g *gin.RouterGroup) {
-	g.GET("/cluster/events", h.Wrap(h.ListK8sEvents, acbiz.PathGuard("system", "*", "read")))
+	g.GET("/cluster/events", h.Wrap(h.ListK8sEvents, eventGuard))
 	g.GET("/cluster/nodes", h.Wrap(h.ListK8sNodes, acbiz.PathGuard("system", "*", "read")))
-	g.GET("/cluster/namespaces", h.Wrap(h.ListK8sNamespaces, acbiz.PathGuard("system", "*", "read")))
-	g.GET("/cluster/storageClasses", h.Wrap(h.ListK8sStorageClasses, acbiz.PathGuard("system", "*", "read")))
+	g.GET("/cluster/namespaces", h.Wrap(h.ListK8sNamespaces, k8sResourceGuard))
+	g.GET("/cluster/storageClasses", h.Wrap(h.ListK8sStorageClasses, k8sResourceGuard))
 	g.POST("/cluster/namespaces", h.Wrap(h.CreateK8sNamespace, acbiz.PathGuard("system", "*", "write")))
 }
