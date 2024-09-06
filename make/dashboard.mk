42ce5faa3bc939e184d685182ba98b6c5ccc636f
yuyi <yuyi.wsy@oceanbase.com>
2024-09-06 16:13:52 +0800

    chore(release): bump version of dashboard chart

--- make/dashboard.mk
+++ make/dashboard.mk
@@ -4,7 +4,7 @@ PROJECT=oceanbase-dashboard
 PROCESSOR=4
 PWD ?= $(shell pwd)
 
-DASHBOARD_VERSION ?= 0.3.0
+DASHBOARD_VERSION ?= 0.3.1
 DASHBOARD_IMG ?= quay.io/oceanbase/oceanbase-dashboard:${DASHBOARD_VERSION}
 COMMIT_HASH ?= $(shell git rev-parse --short HEAD)
 BUILD_TIMESTAMP ?= $(shell date '+%Y%m%d%H%M%S')
