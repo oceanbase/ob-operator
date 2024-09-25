0e0696e6f0c6f2bb18c0d89094f5f5b62686b3c9
ZmFrZSBwbGFzdGljIHRyZWU= <58327725+lizzy-0323@users.noreply.github.com>
2024-09-25 21:01:53 +0800

    chore: add CI workflows for ob-operator cli tool (#570)

--- make/cli.mk
+++ make/cli.mk
@@ -1,12 +1,27 @@
 ##@ cli
 
-PROJECT=oceanbase-cli
-PROCESSOR=4
-PWD ?= $(shell pwd)
+BINARY_NAME ?= obocli
+CLI_VERSION ?= 0.1.0
+GOARCH ?=$(shell uname -m)
+GOOS ?= $(shell uname -s | tr LD ld)
+
+# If GOARCH not specified, set GOARCH based on the detected architecture
+ifeq ($(GOARCH),x86_64)
+    GOARCH = amd64
+else ifeq ($(GOARCH),aarch64)
+    GOARCH = arm64
+endif
+
+# build args
+CLI_BUILD := GO11MODULE=ON CGO_ENABLED=0 GOOS=$(GOOS) GOARCH=$(GOARCH) go build
+
+# build dir for obocli 
+BUILD_DIR?=bin/
 
 .PHONY: cli-build
-cli-build: cli-bindata-gen cli-dep-install # Build oceanbase-cli
-	go build -o bin/obocli cmd/cli/main.go
+cli-build: cli-dep-install cli-bindata-gen # Build oceanbase-cli
+	@echo "Building $(BINARY_NAME) for $(GOOS)/$(GOARCH)..."
+	$(CLI_BUILD) -o $(BUILD_DIR)$(BINARY_NAME) cmd/cli/main.go
 
 .PHONY: cli-bindata-gen
 cli-bindata-gen: # Generate bindata
@@ -14,14 +29,14 @@ cli-bindata-gen: # Generate bindata
 
 .PHONY: cli-clean
 cli-clean: # Clean build
-	rm -rf bin/obocli
+	rm -rf $(RELEASE_DIR)/$(BINARY_NAME)
 	go clean -i ./...
 
 .PHONY : cli-dep-install
 cli-dep-install: # Install oceanbase-cli deps
 	go install github.com/spf13/cobra
 	go install github.com/go-bindata/go-bindata/...@v3.1.2+incompatible
+	
 .PHONY : cli-run
 cli-run: ## Run oceanbase-cli in dev mode
-	go run ./cmd/cli/main.go
-
+	go run -p 4 ./cmd/cli/main.go
