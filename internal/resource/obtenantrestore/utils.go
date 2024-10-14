7e136185b336a44dfafc0c7451854b73f14a95d4
yuyi <yuyi.wsy@oceanbase.com>
2024-10-14 14:44:12 +0800

    chore(restore): supplement restoring features

--- internal/resource/obtenantrestore/utils.go
+++ internal/resource/obtenantrestore/utils.go
@@ -21,6 +21,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/oceanbase/ob-operator/api/constants"
+	apitypes "github.com/oceanbase/ob-operator/api/types"
 	v1alpha1 "github.com/oceanbase/ob-operator/api/v1alpha1"
 	oceanbaseconst "github.com/oceanbase/ob-operator/internal/const/oceanbase"
 	resourceutils "github.com/oceanbase/ob-operator/internal/resource/utils"
@@ -49,25 +50,8 @@ func (m *ObTenantRestoreManager) getSourceUri() (string, error) {
 		return source.SourceUri, nil
 	}
 	var bakPath, archivePath string
-	if source.BakDataSource != nil && source.BakDataSource.Type == constants.BackupDestTypeOSS {
-		accessId, accessKey, err := m.readAccessCredentials(source.BakDataSource.OSSAccessSecret)
-		if err != nil {
-			return "", err
-		}
-		bakPath = strings.Join([]string{source.BakDataSource.Path, "access_id=" + accessId, "access_key=" + accessKey}, "&")
-	} else {
-		bakPath = "file://" + path.Join(oceanbaseconst.BackupPath, source.BakDataSource.Path)
-	}
-
-	if source.ArchiveSource != nil && source.ArchiveSource.Type == constants.BackupDestTypeOSS {
-		accessId, accessKey, err := m.readAccessCredentials(source.ArchiveSource.OSSAccessSecret)
-		if err != nil {
-			return "", err
-		}
-		archivePath = strings.Join([]string{source.ArchiveSource.Path, "access_id=" + accessId, "access_key=" + accessKey}, "&")
-	} else {
-		archivePath = "file://" + path.Join(oceanbaseconst.BackupPath, source.ArchiveSource.Path)
-	}
+	bakPath = m.getDestPath(source.BakDataSource)
+	archivePath = m.getDestPath(source.ArchiveSource)
 
 	if bakPath == "" || archivePath == "" {
 		return "", errors.New("Unexpected error: both bakPath and archivePath must be set")
@@ -76,16 +60,27 @@ func (m *ObTenantRestoreManager) getSourceUri() (string, error) {
 	return strings.Join([]string{bakPath, archivePath}, ","), nil
 }
 
-func (m *ObTenantRestoreManager) readAccessCredentials(secretName string) (accessId, accessKey string, err error) {
+func (m *ObTenantRestoreManager) getDestPath(dest *apitypes.BackupDestination) string {
+	if dest.Type == constants.BackupDestTypeNFS || resourceutils.IsZero(dest.Type) {
+		return "file://" + path.Join(oceanbaseconst.BackupPath, dest.Path)
+	}
+	if dest.OSSAccessSecret == "" {
+		return ""
+	}
 	secret := &v1.Secret{}
-	err = m.Client.Get(m.Ctx, types.NamespacedName{
-		Namespace: m.Resource.Namespace,
-		Name:      secretName,
+	err := m.Client.Get(m.Ctx, types.NamespacedName{
+		Namespace: m.Resource.GetNamespace(),
+		Name:      dest.OSSAccessSecret,
 	}, secret)
 	if err != nil {
-		return "", "", err
+		m.PrintErrEvent(err)
+		return ""
 	}
-	accessId = string(secret.Data["accessId"])
-	accessKey = string(secret.Data["accessKey"])
-	return accessId, accessKey, nil
+	destPath := strings.Join([]string{dest.Path, "access_id=" + string(secret.Data["accessId"]), "access_key=" + string(secret.Data["accessKey"])}, "&")
+	if dest.Type == constants.BackupDestTypeCOS {
+		destPath += ("&appid=" + string(secret.Data["appId"]))
+	} else if dest.Type == constants.BackupDestTypeS3 {
+		destPath += ("&s3_region=" + string(secret.Data["s3Region"]))
+	}
+	return destPath
 }
