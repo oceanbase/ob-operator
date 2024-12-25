f3613a024008ca4dfea752b025223f347b5dd8ce
yuyi <yuyi.wsy@oceanbase.com>
2024-12-25 18:04:34 +0800

    fix(test): set kubebuilder_assets env var

--- make/development.mk
+++ make/development.mk
@@ -36,6 +36,7 @@ test-package:
 
 .PHONY: test-internal
 test-internal:
+	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) --bin-dir $(LOCALBIN) -p path)" \
 	go test -v ./internal/cli/... \
 	./internal/clients/... \
 	./internal/cmds/... \
