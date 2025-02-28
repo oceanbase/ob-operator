7735862fb48f7460971421ccf0de6247a19714b2
chris-sun-star <hongxin.sun.vip@gmail.com>
2025-02-28 06:13:52 +0000

    commit -am get pods info

--- internal/clients/obcluster.go
+++ internal/clients/obcluster.go
@@ -155,3 +155,13 @@ func ListOBServersOfOBZone(ctx context.Context, obzone *v1alpha1.OBZone) (*v1alp
 	}
 	return &observerList, nil
 }
+
+func GetPodOfOBServer(ctx context.Context, observer *v1alpha1.OBServer) (*corev1.Pod, error) {
+	// this label always exists
+	client := client.GetClient()
+	pod, err := client.ClientSet.CoreV1().Pods(observer.Namespace).Get(ctx, observer.Name, metav1.GetOptions{})
+	if err != nil {
+		return nil, errors.Wrap(err, "get pod")
+	}
+	return pod, nil
+}
