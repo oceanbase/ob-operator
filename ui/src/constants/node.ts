395f68f85886b5307f35fefdc0e3abc4393bcc15
TianWuwt <759644414@qq.com>
2025-03-17 15:40:28 +0800

    feat：优化节点列表及增加相关操作 || feat: Optimize the node list and add related operations (#765)
    * feat：优化节点列表及增加相关操作
    
    * improve：修改注释代码

---
+++ ui/src/constants/node.ts
@@ -0,0 +1,24 @@
+export const OPERATOR_LIST = [
+  {
+    value: 'Equal',
+    label: 'Equal',
+  },
+  {
+    value: 'Exists',
+    label: 'Exists',
+  },
+];
+export const EFFECT_LIST = [
+  {
+    value: 'NoSchedule',
+    label: 'NoSchedule',
+  },
+  {
+    value: 'PerferNoSchedule',
+    label: 'PerferNoSchedule',
+  },
+  {
+    value: 'NoExecute',
+    label: 'NoExecute',
+  },
+];
