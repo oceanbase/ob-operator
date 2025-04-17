a9aabbe5c065ea4238bd429aaa790b08f7ec0e66
Chris Sun <hongxin.sun.vip@gmail.com>
2025-04-17 10:45:19 +0800

    make replica type take effect when add or modify tenant unit (#789)

--- internal/dashboard/business/oceanbase/obtenant_pool.go
+++ internal/dashboard/business/oceanbase/obtenant_pool.go
@@ -63,13 +63,17 @@ func CreateTenantPool(ctx context.Context, nn param.TenantPoolName, p *param.Ten
 		maxIops = int(p.UnitConfig.MaxIops)
 	}
 
+	replicaType := "Full"
+	if p.Type != "" {
+		replicaType = p.Type
+	}
 	for _, zone := range clusterCR.Spec.Topology {
 		if zone.Zone == nn.ZoneName {
 			tenantCR.Spec.Pools = append(tenantCR.Spec.Pools, v1alpha1.ResourcePoolSpec{
 				Zone:     nn.ZoneName,
 				Priority: p.Priority,
 				Type: &v1alpha1.LocalityType{
-					Name:     "Full",
+					Name:     replicaType,
 					Replica:  1,
 					IsActive: true,
 				},
@@ -149,6 +153,9 @@ func PatchTenantPool(ctx context.Context, nn param.TenantPoolName, p *param.Tena
 	for i, pool := range tenantCR.Spec.Pools {
 		if pool.Zone == nn.ZoneName {
 			tenantCR.Spec.Pools[i].Priority = p.Priority
+			if p.Type != "" {
+				tenantCR.Spec.Pools[i].Type.Name = p.Type
+			}
 			if tenantCR.Spec.Pools[i].UnitConfig != nil {
 				if cpuCount, err := resource.ParseQuantity(p.UnitConfig.CPUCount); err == nil {
 					tenantCR.Spec.Pools[i].UnitConfig.MaxCPU = cpuCount
