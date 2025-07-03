25725ffd922f060c8cd411efdd7b01b156d87e7d
chris-sun-star <hongxin.sun.vip@gmail.com>
2025-07-03 17:27:39 +0800

    implement job apis

--- internal/dashboard/handler/inspection_handler.go
+++ internal/dashboard/handler/inspection_handler.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2023 OceanBase
+Copyright (c) 2025 OceanBase
 ob-operator is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
@@ -61,7 +61,7 @@ func CreateOrUpdateInspectionPolicy(c *gin.Context) (*inspection.Policy, error) 
 	if err := c.ShouldBindJSON(policy); err != nil {
 		return nil, errors.NewBadRequest(err.Error())
 	}
-	err := insbiz.CreateOrUpdateInspectionPolicy(c.Request.Context(), *policy)
+	err := insbiz.CreateOrUpdateInspectionPolicy(c.Request.Context(), policy)
 	return policy, err
 }
 
