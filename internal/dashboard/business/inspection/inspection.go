1fa128ad79af99f5cbb9c62b8f98fb38fe28fd9e
chris-sun-star <hongxin.sun.vip@gmail.com>
2025-07-09 05:16:16 +0000

    create cronjobs in the namespace of obcluster

--- internal/dashboard/business/inspection/inspection.go
+++ internal/dashboard/business/inspection/inspection.go
@@ -16,7 +16,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"os"
 
 	"github.com/pkg/errors"
 
@@ -80,7 +79,6 @@ func newPolicyFromCronJob(ctx context.Context, cronJob *batchv1.CronJob) (*insmo
 }
 
 func listInspectionCronJobs(ctx context.Context, namespace, name, obcluster, scenario string) ([]batchv1.CronJob, error) {
-	jobNamespace := os.Getenv("NAMESPACE")
 	client := client.GetClient()
 	listOptions := metav1.ListOptions{}
 	labelSelector := fmt.Sprintf("%s=%s,%s=%s", insconst.INSPECTION_LABEL_MANAGED_BY, bizconst.DASHBOARD_APP_NAME, insconst.INSPECTION_LABEL_JOB_TYPE, insconst.JOB_TYPE_INSPECTION)
@@ -97,7 +95,7 @@ func listInspectionCronJobs(ctx context.Context, namespace, name, obcluster, sce
 		labelSelector = fmt.Sprintf("%s,%s=%s", labelSelector, insconst.INSPECTION_LABEL_SCENARIO, scenario)
 	}
 	listOptions.LabelSelector = labelSelector
-	cronJobList, err := client.ClientSet.BatchV1().CronJobs(jobNamespace).List(ctx, listOptions)
+	cronJobList, err := client.ClientSet.BatchV1().CronJobs("").List(ctx, listOptions)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to list cron jobs")
 	}
@@ -165,7 +163,6 @@ func DeleteInspectionPolicy(ctx context.Context, namespace, name, scenario strin
 }
 
 func createCronJobForInspection(ctx context.Context, obclusterMeta *response.OBClusterMetaBasic, scheduleConfig *insmodel.InspectionScheduleConfig) error {
-	jobNamespace := os.Getenv("NAMESPACE")
 	cronJobName := fmt.Sprintf("ins-%s-%s", scheduleConfig.Scenario, rand.String(6))
 	pvcName := "pvc-" + cronJobName
 	configVolumeName := "config"
@@ -240,19 +237,17 @@ func createCronJobForInspection(ctx context.Context, obclusterMeta *response.OBC
 		},
 	}
 
-	controller := true
 	ownerRef := metav1.OwnerReference{
 		APIVersion: v1alpha1.GroupVersion.String(),
 		Kind:       "OBCluster",
 		Name:       obclusterMeta.Name,
 		UID:        types.UID(obclusterMeta.UID),
-		Controller: &controller,
 	}
 
 	cronJob := &batchv1.CronJob{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:            cronJobName,
-			Namespace:       jobNamespace,
+			Namespace:       obclusterMeta.Namespace,
 			Labels:          labels,
 			OwnerReferences: []metav1.OwnerReference{ownerRef},
 		},
@@ -262,7 +257,7 @@ func createCronJobForInspection(ctx context.Context, obclusterMeta *response.OBC
 	pvc := &corev1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:            pvcName,
-			Namespace:       jobNamespace,
+			Namespace:       obclusterMeta.Namespace,
 			OwnerReferences: []metav1.OwnerReference{ownerRef},
 		},
 		Spec: corev1.PersistentVolumeClaimSpec{
@@ -277,22 +272,28 @@ func createCronJobForInspection(ctx context.Context, obclusterMeta *response.OBC
 		},
 	}
 
+	logger.Infof("Create pvc for cronjob %s", cronJobName)
 	client := client.GetClient()
-	_, err := client.ClientSet.CoreV1().PersistentVolumeClaims(jobNamespace).Create(ctx, pvc, metav1.CreateOptions{})
+	pvcObject, err := client.ClientSet.CoreV1().PersistentVolumeClaims(obclusterMeta.Namespace).Create(ctx, pvc, metav1.CreateOptions{})
 	if err != nil {
 		return errors.Wrap(err, "Failed to create pvc")
 	}
+	logger.Infof("Successfully created pvc %v", pvcObject)
 
-	_, err = client.ClientSet.BatchV1().CronJobs(jobNamespace).Create(ctx, cronJob, metav1.CreateOptions{})
-	return err
+	logger.Infof("Create cronjob %s", cronJobName)
+	cronjobObject, err := client.ClientSet.BatchV1().CronJobs(obclusterMeta.Namespace).Create(ctx, cronJob, metav1.CreateOptions{})
+	if err != nil {
+		return errors.Wrap(err, "Failed to create cronjob")
+	}
+	logger.Infof("Successfully created cronjob %v", cronjobObject)
+	return nil
 }
 
 func updateCronJobForInspection(ctx context.Context, cronJob *batchv1.CronJob, suspend bool, scheduleConfig *insmodel.InspectionScheduleConfig) error {
-	jobNamespace := os.Getenv("NAMESPACE")
 	cronJob.Spec.Suspend = &suspend
 	cronJob.Spec.Schedule = scheduleConfig.Schedule
 	client := client.GetClient()
-	_, err := client.ClientSet.BatchV1().CronJobs(jobNamespace).Update(ctx, cronJob, metav1.UpdateOptions{})
+	_, err := client.ClientSet.BatchV1().CronJobs("").Update(ctx, cronJob, metav1.UpdateOptions{})
 	return err
 }
 
@@ -351,7 +352,6 @@ func TriggerInspection(ctx context.Context, namespace, name, scenario string) (*
 
 func listInspectionJobs(ctx context.Context, namespace, name, obcluster, scenario string) ([]batchv1.Job, error) {
 
-	jobNamespace := os.Getenv("NAMESPACE")
 	client := client.GetClient()
 	listOptions := metav1.ListOptions{}
 	labelSelector := fmt.Sprintf("%s=%s,%s=%s", insconst.INSPECTION_LABEL_MANAGED_BY, bizconst.DASHBOARD_APP_NAME, insconst.INSPECTION_LABEL_JOB_TYPE, insconst.JOB_TYPE_INSPECTION)
@@ -368,7 +368,7 @@ func listInspectionJobs(ctx context.Context, namespace, name, obcluster, scenari
 		labelSelector = fmt.Sprintf("%s,%s=%s", labelSelector, insconst.INSPECTION_LABEL_SCENARIO, scenario)
 	}
 	listOptions.LabelSelector = labelSelector
-	jobList, err := client.ClientSet.BatchV1().Jobs(jobNamespace).List(ctx, listOptions)
+	jobList, err := client.ClientSet.BatchV1().Jobs("").List(ctx, listOptions)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to list jobs")
 	}
@@ -393,9 +393,8 @@ func ListInspectionReports(ctx context.Context, namespace, name, obcluster, scen
 }
 
 func GetInspectionReport(ctx context.Context, id string) (*insmodel.Report, error) {
-	jobNamespace := os.Getenv("NAMESPACE")
 	client := client.GetClient()
-	job, err := client.ClientSet.BatchV1().Jobs(jobNamespace).Get(ctx, id, metav1.GetOptions{})
+	job, err := client.ClientSet.BatchV1().Jobs("").Get(ctx, id, metav1.GetOptions{})
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to get job")
 	}
