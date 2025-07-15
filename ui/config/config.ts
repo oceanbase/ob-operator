9938784e75dbf9e6b78ff82f8012c35d9710438d
吴甜 <759644414@qq.com>
2025-07-15 17:32:56 +0800

    improve：联调更新

--- ui/config/config.ts
+++ ui/config/config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   jsMinifier: 'terser',
   proxy: {
     '/api/v1': {
-      target: 'http://11.161.204.50:30120',
+      target: 'http://11.161.204.49:30555',
     },
   },
 });
