b81fc3bd8bfa67e051d4fed989e880749a12af21
Chris Sun <hongxin.sun.vip@gmail.com>
2025-07-29 14:57:06 +0800

    Configurations for inspection and jobs (#826)

--- internal/dashboard/business/alarm/diagnose.go
+++ internal/dashboard/business/alarm/diagnose.go
@@ -21,6 +21,7 @@ import (
 
 	bizconst "github.com/oceanbase/ob-operator/internal/dashboard/business/constant"
 	"github.com/oceanbase/ob-operator/internal/dashboard/business/oceanbase"
+	"github.com/oceanbase/ob-operator/internal/dashboard/config"
 	"github.com/oceanbase/ob-operator/internal/dashboard/generated/bindata"
 	"github.com/oceanbase/ob-operator/internal/dashboard/model/alarm/alert"
 	jobmodel "github.com/oceanbase/ob-operator/internal/dashboard/model/job"
@@ -43,7 +44,7 @@ func DiagnoseAlert(ctx context.Context, param *alert.AnalyzeParam) (*jobmodel.Jo
 	jobOutputDir := filepath.Join(sharedMountPath, jobName)
 	configFileName := "config.yaml"
 	configFilePath := filepath.Join(jobOutputDir, configFileName)
-	ttlSecondsAfterFinished := int32(5 * 60)
+	ttlSecondsAfterFinished := config.GetConfig().Job.Normal.TTLSecondsAfterFinished
 
 	labels := map[string]string{
 		bizconst.LABEL_MANAGED_BY:        bizconst.DASHBOARD_APP_NAME,
@@ -103,7 +104,7 @@ func DiagnoseAlert(ctx context.Context, param *alert.AnalyzeParam) (*jobmodel.Jo
 				InitContainers: []corev1.Container{
 					{
 						Name:            "generate-config",
-						Image:           "oceanbase/oceanbase-helper:latest",
+						Image:           config.GetConfig().Inspection.OBHelper.Image,
 						ImagePullPolicy: corev1.PullIfNotPresent,
 						Command:         []string{"bash", "-c", fmt.Sprintf("mkdir -p %s && /home/admin/oceanbase/bin/oceanbase-helper generate obdiag-config -n %s -c %s -o %s", jobOutputDir, obclusterObj.Namespace, obclusterObj.Name, configFilePath)},
 						VolumeMounts: []corev1.VolumeMount{
@@ -117,7 +118,7 @@ func DiagnoseAlert(ctx context.Context, param *alert.AnalyzeParam) (*jobmodel.Jo
 				Containers: []corev1.Container{
 					{
 						Name:            "diagnose",
-						Image:           "oceanbase/obdiag:latest",
+						Image:           config.GetConfig().Inspection.OBDiag.Image,
 						ImagePullPolicy: corev1.PullIfNotPresent,
 						Command:         []string{"/bin/sh", "-c"},
 						Args: []string{
