31fce4d72c4eb82854044b1b5485e59a3afca4c3
chris-sun-star <hongxin.sun.vip@gmail.com>
2026-01-15 16:38:42 +0800

    delete un-related generate step while building operator or helper

--- make/helper.mk
+++ make/helper.mk
@@ -1,6 +1,6 @@
 ##@ Helper
 
 .PHONY: helper
-helper: dashboard-bindata-gen dashboard-doc-gen  ## Build oceanbase helper binary.
+helper: ## Build oceanbase helper binary.
 	@echo "Building helper..."
 	CGO_ENABLED=0 GOOS=linux go build -p $(PROCESSOR) -a -o bin/oceanbase-helper ./cmd/oceanbase-helper/main.go
